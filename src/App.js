d0e43b4c6f2e1622762d3d2d451a855ce6f5430c
Rafael Pantoja <rafaelbp92@gmail.com>
2025-01-21 20:25:06 +0000

    Initial state management with reducer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
-export default function App() {
-  return <div className="app">
-    <Header />
+const initialState = {
+  questions: [],
+  status: "loading" || "error" || "ready" || "active" || "finished",
+};
 
-    <Main>
-      <p>1/15</p>
-      <p>Question?</p>
-    </Main>
-  </div>
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Action unkown");
+  }
+}
+
+export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch("https://6m9hz3-8000.csb.app/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((_error) => {
+        dispatch({ type: "dataFailed" });
+      });
+  }, []);
+  return (
+    <div className="app">
+      <Header />
+
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
+    </div>
+  );
 }
